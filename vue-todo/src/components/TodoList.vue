2f25ab6307dcaba7653750274cc864d6f9565643
조용진 <whp5047@gmail.com>
2022-12-28 13:22:22 +0900

    #1 TodoList 출력 및 삭제 localStorage 에 저장한 item들을 data()에 배열로 받아 v-for을 통한 리스트 출력, @click 이벤트를 통한 해당 localStorage 요소 삭제와 index를 통한 data() 배열요소 삭제

---
+++ vue-todo/src/components/TodoList.vue
@@ -0,0 +1,79 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="(todoItem, index) in todoItems" :key="todoItem" class="shadow">
+                <i class="checkBtn fa-solid fa-check"></i>
+                {{todoItem}}
+                <span class="removeBtn" @click="removeTodo(todoItem, index)"> 
+                    <i class="fa-solid fa-trash-can"></i>
+                </span>
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            todoItems : [],
+        }
+    },
+
+    created() {
+        if(localStorage.length >0) {
+            for(var i =0 ; i< localStorage.length ; i++){
+                this.todoItems.push(localStorage.key(i));
+            }
+        }
+    },
+
+    methods: {
+        removeTodo(todoItem, index){
+            console.log(todoItem, index);
+            localStorage.removeItem(todoItem);
+            this.todoItems.splice(index,1);
+        }
+    },
+}
+</script>
+<style>
+    ul{
+        list-style-type: none;
+        padding-left: 0px;
+        margin-top: 0;
+        text-align: left;
+    }
+
+    li{
+        display: flex;
+        min-height: 50px;
+        height: 50px;
+        line-height: 50px;
+        margin: 0.5rem 0;
+        padding: 0 0.9rem;
+        background: white;
+        border-radius: 5px;
+        color: black;
+    }
+
+    .checkBtn{
+        line-height: 45px;
+        color: #62acde;
+        margin-right: 5px;
+    }
+
+    .checkBtnCompleted{
+        color: #b3adad;
+    }
+
+    .textCompleted{
+        text-decoration: line-through;
+        color: #b3adad;
+    }
+
+    .removeBtn{
+        margin-left: auto;
+        color: #de4343;
+    }
+    
+</style>
