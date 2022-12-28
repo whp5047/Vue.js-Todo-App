b24220acb73d00c012fe6e262d992684276e1669
조용진 <whp5047@gmail.com>
2022-12-28 13:17:08 +0900

    #1 컴포넌트 import 및 배치

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -1,18 +1,49 @@
 <template>
-
+  <div id="App">
+      <todo-header></todo-header>
+      <todo-input></todo-input>
+      <todo-list></todo-list>
+      <todo-footer></todo-footer>
+  </div>
 </template>
 
 <script>
-
+import TodoHeader from './components/TodoHeader.vue'
+import TodoInput from './components/TodoInput.vue'
+import TodoList from './components/TodoList.vue'
+import TodoFooter from './components/TodoFooter.vue'
 
 export default {
   name: 'App',
   components: {
-    
+    TodoHeader,
+    TodoList,
+    TodoInput,
+    TodoFooter,
+
   }
+
 }
 </script>
 
 <style>
+  body{
+    text-align: center;
+    background-color: #F6F6F6;
+  }
+
+  input{
+    border-style: groove;
+    width: 200px;
+  }
+
+  button{
+    border-style: groove;
+    margin: 3px;
+  }
+
+  .shadow{
+    box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.3);
+  }
 
 </style>
